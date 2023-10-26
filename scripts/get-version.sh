4e140dd334f6d282cda0233981f7b0feff283bd6
Joachim Bauch <bauch@struktur.de>
2023-10-26 14:09:26 +0200

    Get version from tag if building from tag.

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -7,8 +7,11 @@ if [ -s "$ROOT/../version.txt" ]; then
     VERSION=$(cat "$ROOT/../version.txt" | tr -d '[:space:]')
 fi
 if [ -z $VERSION ] && [ -d "$ROOT/../.git" ]; then
+    TAG=$(git tag --points-at HEAD | sed 's/v//')
     if [ "$1" == "--tar" ]; then
         VERSION=$(git describe --dirty --tags --always | sed 's/debian\///g')
+    elif [ -n "$TAG" ]; then
+        VERSION="$TAG"
     else
         VERSION=$(git log -1 --pretty=%H)
     fi
