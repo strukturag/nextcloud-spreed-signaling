aa7ca9d02fd5bcaf3332c60a0c50a6ec7d3ca85b
Joachim Bauch <bauch@struktur.de>
2026-03-12 10:57:12 +0100

    Move tools helpers to internal package.

--- internal/tools/vendor_helper_test.go
+++ internal/tools/vendor_helper_test.go
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package signaling
+package tools
 
 // Import modules that would otherwise not be detected by "go mod vendor".
 import (
